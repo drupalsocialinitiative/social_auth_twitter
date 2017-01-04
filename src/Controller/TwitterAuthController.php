39b070529de8e8d3a27b927a227e472f38a45145
Utkarsh Dixit <denutkarsh@gmail.com>
2017-01-04 22:29:51 +0530

    Initial Commit

---
+++ src/Controller/TwitterAuthController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Drupal\social_auth_twitter\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\social_auth_twitter\TwitterAuthManager;
+use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_auth\SocialAuthUserManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Zend\Diactoros\Response\RedirectResponse;
+
+/**
+ * Manages requests to Twitter API.
+ */
+class TwitterAuthController extends ControllerBase {
+
+  /**
+   * The network plugin manager.
+   *
+   * @var \Drupal\social_api\Plugin\NetworkManager
+   */
+  private $networkManager;
+
+  /**
+   * The Twitter authentication manager.
+   *
+   * @var \Drupal\social_auth_twitter\TwitterAuthManager
+   */
+  private $twitterManager;
+
+  /**
+   * The user manager.
+   *
+   * @var \Drupal\social_auth\SocialAuthUserManager
+   */
+  private $userManager;
+
+  /**
+   * TwitterLoginController constructor.
+   *
+   * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
+   *   Used to get an instance of social_auth_twitter network plugin.
+   * @param \Drupal\social_auth_twitter\TwitterAuthManager $twitter_manager
+   *   Used to manage authentication methods.
+   * @param \Drupal\social_auth\SocialAuthUserManager $user_manager
+   *   Manages user login/registration.
+   */
+  public function __construct(NetworkManager $network_manager, TwitterAuthManager $twitter_manager, SocialAuthUserManager $user_manager) {
+    $this->networkManager = $network_manager;
+    $this->twitterManager = $twitter_manager;
+    $this->userManager = $user_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('plugin.network.manager'),
+      $container->get('twitter_auth.manager'),
+      $container->get('social_auth.user_manager')
+    );
+  }
+
+  /**
+   * Redirect to Twitter Services Authentication page.
+   *
+   * @return \Zend\Diactoros\Response\RedirectResponse
+   *   Redirection to Twitter Accounts.
+   */
+  public function redirectToTwitter() {
+    /* @var TwitterAuth $network_plugin */
+    // Creates an instance of the social_auth_twitter Network Plugin.
+    $network_plugin = $this->networkManager->createInstance('social_auth_twitter');
+
+    /* @var TwitterOAuth $connection */
+    /* Gets the Twitter SDK.
+     *
+     * Notice that getSdk() does not require any argument, whereas getSdk2()
+     * does.
+     *
+     * Your social network might not require to have different ways of getting
+     * an instance of the SDK, but Twitter does.
+     */
+    $connection = $network_plugin->getSdk();
+
+    // Requests Twitter to get temporary tokens.
+    $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => $network_plugin->getOauthCallback()));
+
+    // Saves the temporary token values in session.
+    $this->twitterManager->setOauthToken($request_token['oauth_token']);
+    $this->twitterManager->setOauthTokenSecret($request_token['oauth_token_secret']);
+
+    // Generates url for user authentication.
+    $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+
+    // Redirects the user to allow him to grant permissions.
+    return new RedirectResponse($url);
+  }
+
+  /**
+   * Callback function to login user.
+   */
+  public function callback() {
+    $oauth_token = $this->twitterManager->getOauthToken();
+    $oauth_token_secret = $this->twitterManager->getOauthTokenSecret();
+    /* @var \Abraham\TwitterOAuth\TwitterOAuth $client */
+    $client = $this->networkManager->createInstance('social_auth_twitter')->getSdk2($oauth_token, $oauth_token_secret);
+
+    $this->twitterManager->setClient($client);
+
+    // Gets the permanent access token.
+    $access_token = $client->oauth('oauth/access_token', array('oauth_verifier' => $this->twitterManager->getOauthVerifier()));
+    $connection = $this->networkManager->createInstance('social_auth_twitter')->getSdk2($access_token['oauth_token'], $access_token['oauth_token_secret']);
+    $params = array('include_email' => 'true', 'include_entities' => 'false', 'skip_status' => 'true');
+    // Gets user information.
+    $user = $connection->get("account/verify_credentials",$params);
+    // If user information could be retrieved.
+    if ($user) {
+      // Tries to load the user by his email.
+      $drupal_user = $this->userManager->loadUserByProperty('mail', $user->email);
+      // If user email has already an account in the site.
+
+      if ($drupal_user) {
+        if ($this->userManager->loginUser($drupal_user)) {
+          return $this->redirect('user.page');
+        }
+      }
+
+      $drupal_user = $this->userManager->createUser($user->name, $user->email);
+      // If the new user could be registered.
+      if ($drupal_user) {
+        //var_dump($drupal_user);
+        // If the new user could be logged in.
+        if ($this->userManager->loginUser($drupal_user)) {
+          return $this->redirect('user.page');
+        }
+      }
+    }
+    var_dump($user);
+    drupal_set_message($this->t('You could not be authenticated, please contact the administrator'), 'error');
+    return $this->redirect('user.login');
+    
+  }
+
+}
---
+++ src/Plugin/Network/TwitterAuth.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\social_auth_twitter\Plugin\Network;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\MetadataBubblingUrlGenerator;
+use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\SocialAuthNetwork;
+use \Abraham\TwitterOAuth\TwitterOAuth;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines Social Auth Google Network Plugin.
+ *
+ * @Network(
+ *   id = "social_auth_twitter",
+ *   social_network = "Twitter",
+ *   type = "social_auth",
+ *   handlers = {
+ *      "settings": {
+ *          "class": "\Drupal\social_auth_twitter\Settings\TwitterAuthSettings",
+ *          "config_id": "social_auth_twitter.settings"
+ *      }
+ *   }
+ * )
+ */
+class TwitterAuth extends SocialAuthNetwork {
+  /**
+   * The url generator.
+   *
+   * @var \Drupal\Core\Render\MetadataBubblingUrlGenerator
+   */
+  protected $urlGenerator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $container->get('url_generator'),
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * GoogleLogin constructor.
+   *
+   * @param \Drupal\Core\Render\MetadataBubblingUrlGenerator $url_generator
+   *   Used to generate a absolute url for authentication.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(MetadataBubblingUrlGenerator $url_generator, array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
+
+    $this->urlGenerator = $url_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initSdk() {
+    $class_name = '\Abraham\TwitterOAuth\TwitterOAuth';
+    if (!class_exists($class_name)) {
+      throw new SocialApiException(sprintf('The PHP SDK for Twitter Client could not be found. Class: %s.', $class_name));
+    }
+
+    /* @var \Drupal\social_auth_twitter\Settings\TwitterAuthSettings $settings */
+    $settings = $this->settings;
+
+    // Gets the absolute url of the callback.
+    $redirect_uri = $this->urlGenerator->generateFromRoute('social_auth_twitter.callback', array(), array('absolute' => TRUE));
+
+    // Creates a and sets data to TwitterOAuth object.
+    $client = new TwitterOAuth($settings->getConsumerKey(), $settings->getConsumerSecret());
+
+    return $client;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOauthCallback() {
+    return $this->urlGenerator->generateFromRoute('social_auth_twitter.callback', array(), array('absolute' => TRUE));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSdk2($oauth_token, $oauth_token_secret) {
+    /* @var \Drupal\social_post_example\Settings\TwitterPostSettings $settings */
+    $settings = $this->settings;
+
+    return new TwitterOAuth($settings->getConsumerKey(), $settings->getConsumerSecret(),
+                $oauth_token, $oauth_token_secret);
+  }
+
+}
