bb17d04805b80213fb2829302b0b14dca96a09f0
Utkarsh Dixit <downloadplaza97@gmail.com>
2017-01-06 20:52:49 +0530

    Update TwitterAuthSettingsInterface.php

--- src/Settings/TwitterAuthSettingsInterface.php
+++ src/Settings/TwitterAuthSettingsInterface.php
@@ -23,20 +23,4 @@ interface TwitterAuthSettingsInterface {
    */
   public function getConsumerSecret();
 
-  /**
-   * Gets a TwitterOAuth instance with oauth_token and oauth_token_secret.
-   *
-   * This method creates the SDK object by also passing the oauth_token and
-   * oauth_token_secret. It is used for getting permanent tokens from
-   * Twitter and authenticating users that has already granted permission.
-   *
-   * @param string $oauth_token
-   *   The oauth token.
-   * @param string $oauth_token_secret
-   *   The oauth token secret.
-   *
-   * @return \Abraham\TwitterOAuth\TwitterOAuth
-   *   The instance of the connection to Twitter.
-   */
-   public function getSdk2($oauth_token, $oauth_token_secret);
 }
