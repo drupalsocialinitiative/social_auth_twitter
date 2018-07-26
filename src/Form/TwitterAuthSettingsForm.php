36130629a8440ce85a788562d92f972e453d04c3
Getulio Sánchez <valentin2507@gmail.com>
2018-07-25 22:40:49 -0400

    Issue #2985592: Provide callback URL in settings form

--- src/Form/TwitterAuthSettingsForm.php
+++ src/Form/TwitterAuthSettingsForm.php
@@ -52,6 +52,14 @@ class TwitterAuthSettingsForm extends SocialAuthSettingsForm {
       '#description' => $this->t('Copy the Consumer Secret here'),
     ];
 
+    $form['twitter_settings']['callback_urls'] = [
+      '#type' => 'textfield',
+      '#disabled' => TRUE,
+      '#title' => $this->t('Callback URLs'),
+      '#description' => $this->t('Copy this value to <em>Callback URLs</em> field of your Twitter App settings.'),
+      '#default_value' => $GLOBALS['base_url'] . '/user/login/twitter/callback',
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
